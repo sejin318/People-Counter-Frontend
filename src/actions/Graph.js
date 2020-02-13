473c502db130cda31c689285dbe7aa3606a64baf
Sejin Park <sejin97318@gmail.com>
2020-02-13 19:46:59 +0900

    added time

--- src/actions/Graph.js
+++ src/actions/Graph.js
@@ -26,40 +26,43 @@ const updateImage = (data, location, error) => ({
 export const fetchData = () => {
   return async (dispatch, getState) => {
     // initializing the data
-    for(let i = 0; i < location_list.length; i++){
-      const location = location_list[i];
-      const init_url = location.replace('realtime', 'init');
-      axios.get(init_url)
-      .then( (e) => {
-        console.log("response", e);
-        const data_list = JSON.parse(`{ "data": ${e.data} }`).data;
-        for(let i = 0; i < data_list.length; i++){
-          const data = data_list[i];
-          const img = data.img_data;
-          const time = data.datetime;
-          const count = data.count;
-          if(img === undefined){
-            dispatch(updateImage(null, location, true));
-          } else {
-            dispatch(updateImage(img, location, false));
-          }
-          if(data.datetime !== undefined && data.count !== undefined){
-            dispatch(receiveData({ time: time, count: count }, location, false));
-          } else {
-            dispatch(receiveData({}, location, true));
-          }
-        }
-      })
-      .catch( (error) => {
-        console.log(error);
-      });
-    }
+    // for(let i = 0; i < location_list.length; i++){
+    //   const location = location_list[i];
+    //   const init_url = location.replace('realtime', 'init');
+    //   axios.get(init_url)
+    //   .then( (e) => {
+    //     console.log("response", e);
+    //     const data_list = JSON.parse(`{ "data": ${e.data} }`).data;
+    //     for(let i = 0; i < data_list.length; i++){
+    //       const data = data_list[i];
+    //       const img = data.img_data;
+    //       const time = data.datetime;
+    //       const count = data.count;
+    //       if(img === undefined){
+    //         dispatch(updateImage(null, location, true));
+    //       } else {
+    //         dispatch(updateImage(img, location, false));
+    //       }
+    //       if(data.datetime !== undefined && data.count !== undefined){
+    //         dispatch(receiveData({ time: time, count: count }, location, false));
+    //       } else {
+    //         dispatch(receiveData({}, location, true));
+    //       }
+    //     }
+    //   })
+    //   .catch( (error) => {
+    //     console.log(error);
+    //   });
+    // }
+
+
 
     // realtime updating data
     for(let i = 0; i < location_list.length; i++){
       const location = location_list[i];
       const eventSource = new EventSource(API_URL[location]);
       eventSource.onmessage = e => {
+        console.log("message received at: ", new Date())
         console.log(e);
         const message = `{ "data": ${e.data} }`;
         console.log(message);
@@ -69,7 +72,7 @@ export const fetchData = () => {
           const img = data.img_data;
           const time = data.datetime;
           const count = data.count;
-          if(img === undefined){
+          if(img === undefined){ka
             dispatch(updateImage(null, location, true));
           } else {
             dispatch(updateImage(img, location, false));
