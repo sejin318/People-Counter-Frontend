8e55536a09902c71d7e6982d9fa3138ce7cb300e
Sejin Park <sejin97318@gmail.com>
2020-02-22 06:22:59 +0900

    tiny error fix

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,29 +1,24 @@
 import React from 'react';
+import WindowSize, { useWindowSize } from "@reach/window-size";
 
 export default class Image extends React.Component {
 
   render() {
     const { location, img } = this.props;
     return (
-      <div>
-        <h1 className="mt-5">CAMERA VIEW AT {location.toUpperCase()}</h1>
-        <img width="1600" height="900" src={`data:image/jpeg;base64,${img}`} />
-      </div>
+      <WindowSize>
+      {
+        function(size){
+          console.log(size, size.width*0.5, size.height*0.5);
+          return (
+            <div>
+              <h1 className="mt-5">CAMERA VIEW AT {location.toUpperCase()}</h1>
+              <img width={size.width*0.5} height={size.height*0.5} src={`data:image/jpeg;base64,${img}`} />
+            </div>
+          );
+        }
+      }
+      </WindowSize>
     )
   }
 }
-
-
-<WindowSize>
-{
-  function(size){
-    console.log(size, size.width*0.5, size.height*0.5);
-    return (
-      <div>
-        <h1 className="mt-5">CAMERA VIEW AT {location.toUpperCase()}</h1>
-        <img width={size.width*0.5} height={size.height*0.5} src={`data:image/jpeg;base64,${img}`} />
-      </div>
-    );
-  }
-}
-</WindowSize>
