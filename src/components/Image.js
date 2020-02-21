6ea5690581f6844fae4b0544ec27206130579dc4
Sejin Park <sejin97318@gmail.com>
2020-02-22 06:21:55 +0900

    tiny error fix

--- src/components/Image.js
+++ src/components/Image.js
@@ -12,3 +12,18 @@ export default class Image extends React.Component {
     )
   }
 }
+
+
+<WindowSize>
+{
+  function(size){
+    console.log(size, size.width*0.5, size.height*0.5);
+    return (
+      <div>
+        <h1 className="mt-5">CAMERA VIEW AT {location.toUpperCase()}</h1>
+        <img width={size.width*0.5} height={size.height*0.5} src={`data:image/jpeg;base64,${img}`} />
+      </div>
+    );
+  }
+}
+</WindowSize>
