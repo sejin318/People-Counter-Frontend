e3f0ec67db046836f1ae0c8ab99eab3a51c496d1
Sejin Park <sejin97318@gmail.com>
2020-02-22 05:38:31 +0900

    tiny error fix

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -25,7 +25,7 @@ export default class Graph extends React.Component {
       <h1 className="mt-5">People Count at {location.toUpperCase()}</h1>
       <h4>Current Count: {data[data.length-1].count}</h4>
       <Line
-      width="50%"
+      width="10%"
       height="50%"
       data={{
         labels: data.map((item) => (
