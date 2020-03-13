82ce7a19c19d23c4be18df73a6cda7c07c75edef
Sejin Park <sejin97318@gmail.com>
2020-03-14 06:06:36 +0900

    typo

--- src/containers/Image.js
+++ src/containers/Image.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 import Image from '../components/Image';
-import * from '../actions/Image';
+import * as actions from '../actions/Image';
 
 const mapStateToProps = (state, ownProps) => ({
   img: state.Graph.img[ownProps.location],
