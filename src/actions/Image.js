e1c4d40c1ad12732fef0cbb0e96248f939782a5e
Sejin Park <sejin97318@gmail.com>
2020-03-14 13:17:59 +0900

    small error 1

--- src/actions/Image.js
+++ src/actions/Image.js
@@ -16,18 +16,18 @@ export function drawRegion(canvas, coordinates){ // coordinates as an array of x
   const ctx = canvas.getContext("2d");
   // ctx.globalAlpha = 0.2;
   ctx.beginPath();
-  // ctx.moveTo(coordinates[0], coordinates[1]);
-  // for(let i = 2; i < coordinates.length; i+=2){
-  //   ctx.lineTo(coordinates[i], coordinates[i+1]);
-  // }
-  // ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  // ctx.fill();
+  ctx.moveTo(coordinates[0]*1024/3840, coordinates[1]*768/2160);
+  for(let i = 2; i < coordinates.length; i+=2){
+    ctx.lineTo(coordinates[i]*1024/3840, coordinates[i+1]*768/2160);
+  }
+  ctx.fillStyle = 'rgba(255, 0, 0, 0.3)';
+  ctx.fill();
   // ctx.globalAlpha = 1;
 
-  ctx.moveTo(0, 0);
-  ctx.lineTo(960, 0);
-  ctx.lineTo(960, 540);
-  ctx.lineTo(0, 540);
-  ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
-  ctx.fill();
+  // ctx.moveTo(0, 0);
+  // ctx.lineTo(960, 0);
+  // ctx.lineTo(960, 540);
+  // ctx.lineTo(0, 540);
+  // ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+  // ctx.fill();
 }
