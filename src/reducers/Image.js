33e5035fc207da7541e3248ac1524bdd6d93a2e9
Sejin Park <sejin97318@gmail.com>
2020-03-16 01:17:24 +0900

    error fix

--- src/reducers/Image.js
+++ src/reducers/Image.js
@@ -5,31 +5,31 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
-  console.log("Image reducer called??"); 
+  console.log("Image reducer called??");
   switch (action.type) {
-    case 'START_DRAWING': {
-      console.log('reducer says start drawing');
-      // state.openDrawing = true;
-      return {
-        openDrawing: true,
-        ...state
-      };
-    }
-    case 'ADD_LINE': {
-      console.log('reducer says add line');
-      const x = action.payload.x;
-      const y = action.payload.y;
-      state.lines.push(x);
-      state.lines.push(y);
-      return {...state};
-    }
-    case 'RESET': {
-      console.log('reducer says reset');
-      state.lines = [];
-      return {...state};
-    }
-    default: {
-      return state;
-    }
+    case 'START_DRAWING': 
+    console.log('reducer says start drawing');
+    // state.openDrawing = true;
+    return {
+      openDrawing: true,
+      ...state
+    };
+
+    case 'ADD_LINE':
+    console.log('reducer says add line');
+    const x = action.payload.x;
+    const y = action.payload.y;
+    state.lines.push(x);
+    state.lines.push(y);
+    return {...state};
+
+    case 'RESET':
+    console.log('reducer says reset');
+    state.lines = [];
+    return {...state};
+
+    default:
+    return state;
+
   }
 }
