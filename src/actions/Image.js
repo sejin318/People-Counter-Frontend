978c4f9cce0c26a5b932004f8b5f5a910e338306
Sejin Park <sejin97318@gmail.com>
2020-03-15 22:20:57 +0900

    small error 1

--- src/actions/Image.js
+++ src/actions/Image.js
@@ -25,23 +25,26 @@ function intersect(
 }
 
 export function customDrawing(e, canvas, openDrawing, lines, dispatch){
-  console.log('custom drawing called', openDrawing); 
+  console.log('custom drawing called', openDrawing);
   const rect = canvas.getBoundingClientRect();
   const x = e.clientX - rect.left;
   const y = e.clientY - rect.top;
   const ctx = canvas.getContext("2d");
   ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
   if(!openDrawing){
+    console.log("opendrawing false! ");
     dispatch(start_drawing);
     ctx.beginPath();
     ctx.moveTo(x, y);
   } else {
+    console.log("opendrawing true!");
     dispatch(add_line(x, y));
     ctx.lineTo(x, y);
   }
 }
 
 function start_drawing(){
+  console.log("start_drawing called!"); 
   return {
     type: 'START_DRAWING',
   };
