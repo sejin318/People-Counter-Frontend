eaff83cc5ddfbcf9f36945a06b428d81d52a9407
Sejin Park <sejin97318@gmail.com>
2020-03-15 15:07:34 +0900

    small error 1

--- src/actions/Image.js
+++ src/actions/Image.js
@@ -31,25 +31,8 @@ export function setCanvas(canvas) {
   };
 }
 
-export function resetCanvas(canvas, img){
-  // const img_elem = <img src={`data:image/jpeg;base64,${img}`} />
+export function drawRegion(canvas, coordinates, bbox=[[330, 580, 330, 580]]){
   const ctx = canvas.getContext("2d");
-  ctx.drawImage(img, 0, 0);
-  // img_elem.onload = () => {
-  //   ctx.drawImage(img, 0, 0);
-  // }
-}
-
-export function addBox(canvas) {
-  // console.log("addBox called!!!!!!!!!!!!!!!!!!!!!!!");
-  const ctx = canvas.getContext("2d");
-  ctx.fillRect(110, 110, 100, 100);
-}
-
-export function drawRegion(canvas, coordinates, bbox=[[330, 580, 330, 580]]){ // coordinates as an array of x,y coordinates (1d)
-  // console.log('coordinate is', coordinates);
-  const ctx = canvas.getContext("2d");
-  // ctx.globalAlpha = 0.2;
   ctx.beginPath();
   ctx.moveTo(coordinates[0], coordinates[1]);
   for(let i = 2; i < coordinates.length; i+=2){
@@ -64,26 +47,17 @@ export function drawRegion(canvas, coordinates, bbox=[[330, 580, 330, 580]]){ //
     let bbox_y = (bbox[i][1]+bbox[i][3])/2;
     for(let j = 0; j < coordinates.length-2; j+=2){
       if(intersect(coordinates[j], coordinates[j+1], coordinates[j+2], coordinates[j+3], 0, 0, bbox_x, bbox_y)){
-        // console.log('intersection occurred at:', coordinates[j]*1024/3840, coordinates[j+1]*768/2160, coordinates[j+2]*1024/3840, coordinates[j+3]*768/2160);
         cross_count++;
       }
     }
     if(intersect(coordinates[0], coordinates[1], coordinates[coordinates.length-2], coordinates[coordinates.length-1], 0, 0, bbox_x, bbox_y)){
-      // console.log('intersection occurred at:', coordinates[0]*1024/3840, coordinates[1]*768/2160, coordinates[coordinates.length-2]*1024/3840, coordinates[coordinates.length-1]*768/2160);
       cross_count++;
     }
     if(cross_count & 1){
       total++;
     }
-    // console.log('cross count is', cross_count);
   }
-  // console.log('total is', total);
-  // ctx.globalAlpha = 1;
-
-  // ctx.moveTo(0, 0);
-  // ctx.lineTo(960, 0);
-  // ctx.lineTo(960, 540);
-  // ctx.lineTo(0, 540);
-  // ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
-  // ctx.fill();
+  ctx.font = "20px Arial";
+  ctx.fillText("People Count: "+total, 850, 25);
+  ctx.strokeRect(840, 15, 100, 50);
 }
--- src/components/Image.js
+++ src/components/Image.js
@@ -2,21 +2,19 @@ import React from 'react';
 import WindowSize, { useWindowSize } from "@reach/window-size";
 import './Image.css';
 import Button from '@material-ui/core/Button';
-import { setCanvas, drawRegion, resetCanvas } from '../actions/Image';
+import { setCanvas, drawRegion } from '../actions/Image';
 export default class Image extends React.Component {
 
 
 
   componentDidMount() {
-    // console.log("image component mounted");
     const { dispatch } = this.props;
     const canvas = this.refs.canvas;
     const img = this.refs.image
     dispatch(setCanvas(canvas));
     const ctx = canvas.getContext("2d");
-    // ctx.scale(.25, .25);
     img.onload = () => {
-      console.log('width and height of image is: ', img.width, img.height);
+      // console.log('width and height of image is: ', img.width, img.height);
       ctx.drawImage(img, 0, 0);
     }
   }
@@ -29,11 +27,17 @@ export default class Image extends React.Component {
     drawRegion(canvas, data);
   }
 
+  resetCanvas(){
+    const canvas = this.refs.canvas;
+    const img = this.refs.image
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+  }
+
 
 
   render() {
     const { location, img, buttons, canvas, regions } = this.props;
-    // console.log(regions);
     return (
       <div>
         <h1 className="mt-5">CAMERA VIEW AT {location.toUpperCase()}</h1>
