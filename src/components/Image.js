71ad835e89333c5810cdb1158085bd7a6f27cf21
Sejin Park <sejin97318@gmail.com>
2020-03-16 01:41:30 +0900

    error fix

--- src/components/Image.js
+++ src/components/Image.js
@@ -69,6 +69,7 @@ export default class Image extends React.Component {
       dispatch(start_drawing());
       ctx.beginPath();
       ctx.moveTo(x, y);
+      ctx.lineTo(2*x, 2*y);
     } else {
       console.log("opendrawing true!");
       dispatch(add_line(x, y));
