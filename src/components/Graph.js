03dd7db412c5520232be06887d73159ec1d53025
Sejin Park <sejin97318@gmail.com>
2020-04-19 18:34:15 +0900

    graph resizing fix5

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -27,7 +27,7 @@ export default class Graph extends React.Component {
         <h4>Current Count: {data[data.length-1].count}</h4>
         <Line
         width={200}
-        height={10}
+        height={50}
         data={{
           labels: data.map((item) => (
             item.time == null ?
