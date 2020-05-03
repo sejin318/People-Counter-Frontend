516a6924b86577b1ad687cb32d1910b99ac15a35
Sejin Park <sejin97318@gmail.com>
2020-05-03 20:28:29 +0900

    bug fix2

--- src/components/query.js
+++ src/components/query.js
@@ -81,7 +81,7 @@ export default class Query extends React.Component {
     // remove later
     console.log('request is: ', { start_date: moment(this.props.start_date).format('MM/DD/YYYY, HH:mm:ss'),
             end_date: moment(this.props.end_date).format('MM/DD/YYYY, HH:mm:ss'),
-            target_loc: target_loc
+            target_loc: target_loc, 
             num_loc: this.props.locations.length
     });
 
