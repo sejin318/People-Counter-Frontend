1b10bbea9b008abf813d7c4709a8a54d20810134
Sejin Park <sejin97318@gmail.com>
2020-05-03 13:50:24 +0900

    fixed refdata

--- src/components/query.js
+++ src/components/query.js
@@ -31,7 +31,8 @@ export default class Query extends React.Component {
 
   handleChange(e) {
     const { dispatch } = this.props;
-    dispatch(set_locations(e.target.value));
+    console.log(e.target.value);
+    // dispatch(set_locations(e.target.value));
   }
 
   render() {
