b83f905371477f0c3044be26a57049d50180d941
Sejin Park <sejin97318@gmail.com>
2020-05-03 15:33:57 +0900

    debug

--- src/reducers/query.js
+++ src/reducers/query.js
@@ -6,6 +6,7 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
+  console.log(state); 
   console.log(action);
   switch (action.type) {
     case 'RECEIVE_LOCATION':
