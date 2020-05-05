fa7b9ff3e35c6bb51c04c949c513ee15861337a2
Sejin Park <sejin97318@gmail.com>
2020-05-05 14:54:42 +0900

    bug fixed

--- src/components/query.js
+++ src/components/query.js
@@ -77,6 +77,7 @@ export default class Query extends React.Component {
 
   handleQuerySubmit(){
     if(this.props.start_date > this.props.end_date){
+      console.log('start date greater than enddate!'); 
       this.props.dispatch({
         type: 'SET_OPEN',
         payload: 'The starting datetime should not be after the ending datetime. Please correct the input and try again.'
