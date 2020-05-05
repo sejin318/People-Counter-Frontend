323cc364bb064ed86c2bf5e3e987c0362d7696b3
Sejin Park <sejin97318@gmail.com>
2020-05-05 17:00:12 +0900

    debug

--- src/components/popover.js
+++ src/components/popover.js
@@ -2,6 +2,16 @@ import React from 'react';
 import Popover from '@material-ui/core/Popover';
 import Typography from '@material-ui/core/Typography';
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  popover: {
+    pointerEvents: 'none',
+  },
+  paper: {
+    padding: theme.spacing(1),
+  },
+}));
 
 export default function MouseOverPopover(props) {
   // const [anchorEl, setAnchorEl] = React.useState(null);
