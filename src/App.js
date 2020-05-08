224ed65f3d535cd949cd994e5371fff4ddcd1349
Sejin Park <sejin97318@gmail.com>
2020-05-08 18:26:24 +0900

    debug

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class App extends Component {
                 orientation="horizontal"
                 color="primary"
                 variant="contained"
-                bottom: 0
+                bottom=0
               >
                 <Buttons />
               </ButtonGroup>
